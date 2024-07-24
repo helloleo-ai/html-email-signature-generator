39c047e255dcdaf8613344590e316fd9fae5d306
Terros <augustin@terros.io>
2024-07-24 11:28:52 +0200

    update api endpoint

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-# Update package list and upgrade existing packages
-sudo apt-get update && sudo apt-get upgrade -y
-
-# Install Python 3 and pip if not already installed
-sudo apt-get install -y python3 python3-pip
-
 # Install Python dependencies
 pip3 install -r requirements.txt
 
